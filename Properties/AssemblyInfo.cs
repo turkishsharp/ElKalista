8390e36e647d543270f1f670067b670e52ed565b
turkishsharp <rundll.han@gmail.com>
2015-03-23 18:55:14 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ElKalista(TR)")]
+[assembly: AssemblyTitle("ElKalista")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElKalista(TR)")]
+[assembly: AssemblyProduct("ElKalista")]
 [assembly: AssemblyCopyright("Copyright © jQuery L# 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
